38b7b1ae4a3b0f636fe4506f485595b1fa800ffe
gkruti <67449406+gkruti@users.noreply.github.com>
2020-12-24 14:34:23 -0800

    Add files via upload

---
+++ python-API-challenge/api_keys.py
@@ -0,0 +1,5 @@
+
+weather_api_key = "49e005ca9e1246d4cebaececc6d98c03"
+
+g_key = "AIzaSyCp1mTcaD-jjf9FRZO-kRjktlrsjFNCekk"
+
---
+++ python-API-challenge/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "64576b21ada60b2e33b1a026ac523b26"
+
+# Google API Key
+g_key = "AIzaSyC94jBWW-PMFg83dzEo7UQTRXkBHeG_rDM"
